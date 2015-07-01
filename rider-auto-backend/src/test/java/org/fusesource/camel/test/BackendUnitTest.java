bb83379434c3c3b3b084d92ca010f2a7f2f721f0
joshdreagan <josh.d.reagan@gmail.com>
2015-07-01 02:30:49 -0600

    Fixed unit test.

--- rider-auto-backend/src/test/java/org/fusesource/camel/test/BackendUnitTest.java
+++ rider-auto-backend/src/test/java/org/fusesource/camel/test/BackendUnitTest.java
@@ -6,10 +6,12 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
-import org.apache.camel.test.spring.MockEndpoints;
 import org.fusesource.camel.model.Order;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,6 @@ import org.springframework.test.context.ContextConfiguration;
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:/META-INF/spring/camel-context.xml", 
                        "classpath:/META-INF/spring/test-camel-context.xml"})
-@MockEndpoints
 public class BackendUnitTest {
 
   @Autowired
@@ -27,8 +28,19 @@ public class BackendUnitTest {
   @Produce(uri = "activemq:orders")
   private ProducerTemplate amqPT;
   
-  @EndpointInject(uri = "mock:activemq:orders")
+  @EndpointInject(uri = "mock:bean:backendImpl")
   private MockEndpoint mockBackendEP;
+  
+  @Before
+  public void adviceRoutes() throws Exception {
+    ((ModelCamelContext) camelCtx).getRouteDefinition("rider-auto-backend-route").adviceWith((ModelCamelContext) camelCtx, new AdviceWithRouteBuilder() {
+
+      @Override
+      public void configure() throws Exception {
+        weaveAddLast().to(mockBackendEP);
+      }
+    });
+  }
   
   @Test
   public void testRiderAutoBackendRoute() throws Exception {
